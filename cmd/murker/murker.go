68e5cf6bfe4bf72a6d1677d3043185646f6cee7a
Georgi Atanasov <alfasketer@abv.bg>
2021-01-13 14:15:10 +0200

    Set CORS to Default
    - We set CORS to Default, so requests won't be blocked by CORS policy
    
    Co-authored-by: IvanHristov98 <ivan.hristov.biz@gmail.com>

--- cmd/murker/murker.go
+++ cmd/murker/murker.go
@@ -5,9 +5,10 @@ import (
 	"os"
 	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	apiV1 "github.com/messiah-of-ur/murker/api/v1"
 
+	apiV1 "github.com/messiah-of-ur/murker/api/v1"
 	"github.com/messiah-of-ur/murker/game"
 )
 
@@ -18,6 +19,8 @@ func main() {
 	rand.Seed(time.Now().Unix())
 
 	router := gin.Default()
+	router.Use(cors.Default())
+
 	runner := game.NewGameRunner()
 	registry := apiV1.RoomRegistry{}
 
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/messiah-of-ur/murker
 go 1.15
 
 require (
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
 	github.com/google/uuid v1.1.4
 	github.com/gorilla/websocket v1.4.2
